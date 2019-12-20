5ff0dcb2b6fa093f561e19b81fd12020dfb71cd2
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-12-20 13:24:32 +0100

    risposto al 6° punto

--- Compiti/Program.cs
+++ Compiti/Program.cs
@@ -8,11 +8,17 @@ namespace Compiti
         {
             int a = int.MaxValue;
             Console.WriteLine(a); //2^31 perchè il valore massimo di un int è 2^31 (un bit è per il segno)
-            Console.WriteLine(a + 1);//-2^31 perchè aggiungendo 1 il numero con trentuno 1 diventa negativo
+            try
+            {
+                Console.WriteLine(a + 1); //-2^31 perchè aggiungendo 1 il numero con trentuno 1 diventa negativo
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
             int b = -128;
-            int c = -b;
-            Console.WriteLine(Convert.ToString(b, 2));
-            Console.WriteLine(Convert.ToString(c, 2));
+            int c = -b; //si trasforma in numero positivo
+            Console.WriteLine(Convert.ToString(b, 2)); //si converte in binario il valore b
+            Console.WriteLine(Convert.ToString(c, 2)); //si converte in binario il valore c
             int value = 31;
             int somma = value;
             value = ~value + 1; // NOT
